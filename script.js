ad5f44e4dbe4808091138a8fca197900e40be907
“vukas86” <vukasbit@gmail.com>
2021-10-28 20:40:33 +0200

    smooth scroling small update

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-document.querySelectorAll(".navbar").forEach(function (el) {
-  console.log("hello");
-  el.addEventListener("click", function (e) {
-    e.preventDefault();
+document.querySelector(".navbar").addEventListener("click", function (e) {
+  console.log(e);
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav_link")) {
     const id = e.target.getAttribute("href");
     console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-  });
+  }
 });
 
 const scrollEffect2 = document.querySelector(".btn");
